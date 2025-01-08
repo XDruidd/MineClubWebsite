936db8f43d635c9d1eaccfde138adc876665220b
XDruidd <137764566+XDruidd@users.noreply.github.com>
2025-01-08 20:53:59 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,25 @@ let clic1 = document.querySelector('.clic1');
 let nav_fon = document.querySelector(".nav_fon")
 let animation = document.querySelector('#animation')
 
- 
+const scrollController = {
+    scrollPosition: 0,
+    disabledScroll() {
+      scrollController.scrollPosition = window.scrollY;
+      document.body.style.cssText = `
+        overflow: hidden;
+        position: fixed;
+        top: -${scrollController.scrollPosition}px;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+      `;
+    },
+    enabledScroll() {
+      document.body.style.cssText = '';
+      window.scroll({ top: scrollController.scrollPosition });
+    },
+  };
+  
 clic.onclick = () => {
     if(navigation.style.display == "block"){    
         navigation.style.display = "none";
@@ -27,7 +45,9 @@ clic.onclick = () => {
         animation.animate([
             { opacity: 0, transform: 'scale(0.9)' },
             { opacity: 1, transform: 'scale(1)' }
-        ], {
+        ],
+        scrollController.disabledScroll()
+        ,{
             duration: 300,
             easing: 'ease'
         });
@@ -43,7 +63,9 @@ clic1.onclick = () => {
         animation.animate([
             { opacity: 0, transform: 'scale(1)' },
             { opacity: 1, transform: 'scale(0.8)' }
-        ], {
+        ],
+        scrollController.enabledScroll(),
+        {
             duration: 300,
             easing: 'ease'
         }).onfinish = () => {
